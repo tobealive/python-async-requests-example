a8e109683830d8b77df64095a9202aa9816e38a3
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-02-28 18:47:30 +0100

    refactor handling of test results; add timeout

--- src/async_request.py
+++ src/async_request.py
@@ -3,20 +3,31 @@ from aiohttp import ClientSession
 from typing import TypedDict
 from enum import Enum
 
-url_source = "https://gist.githubusercontent.com/tobealive/b2c6e348dac6b3f0ffa150639ad94211/raw/31524a7aac392402e354bced9307debd5315f0e8/100-popular-urls.txt"
-seperator = "-------------------------------------------------------------------------------"
-
-ResultStatus = Enum('ResultStatus', 'SUCCESS ERROR TIMEOUT PENDING')
-TestResult = dict[str, TypedDict("TestResultData", status=ResultStatus, transferred=int, time=float)]
-Stats = TypedDict("Stats", time=float, successes=int, errors=int, timeouts=int, transferred=float)
-
-results: TestResult = {}
-summary: Stats = {"time": 0, "successes": 0, "errors": 0, "timeouts": 0, "transferred": 0}
-outputs: list[str] = []
-
+url_source = "https://gist.githubusercontent.com/tobealive/b2c6e348dac6b3f0ffa150639ad94211/raw/3db61fe72e1ce6854faa025298bf4cdfd2b2f250/100-popular-urls.txt"
+seperator = '-' * 80
 iterations = 10
 single_source = False
-verbose = False
+verbose = True
+timeout = 5
+
+Stats = TypedDict("Stats", time=float, successes=int, errors=int, timeouts=int, transferred=float)
+ResultStatus = Enum('ResultStatus', 'SUCCESS ERROR PENDING')
+
+
+class TestItem:
+
+	def __init__(self, url: str, status: ResultStatus, transferred: int, time: float):
+		self.url = url
+		self.status = status
+		self.transferred = transferred
+		self.time = time
+
+	def __str__(self):
+		return f"{self.url}, {self.status}, {self.transferred}, {self.time}"
+
+
+summary: Stats = {"time": 0, "successes": 0, "errors": 0, "timeouts": 0, "transferred": 0}
+outputs: list[str] = []
 
 
 def prep_urls() -> list[str]:
@@ -27,57 +38,55 @@ def prep_urls() -> list[str]:
 		return urls
 
 	urls = requests.get(url_source).text.split('\n')
-	urls = list(dict.fromkeys(urls))[:100]  # remove duplicats limit to 100 urls
+	urls = list(dict.fromkeys(urls))[:100]  # remove duplicats, limit to 100 urls
 	return urls
 
 
-async def get_http_resp(url: str, session: ClientSession):
+async def get_http_resp(session: ClientSession, test_item: TestItem) -> TestItem:
 	start_time = time.time()
 
 	try:
-		async with session.get(f"http://www.{url}") as resp:
+		async with session.get(f"http://www.{test_item.url}") as resp:
 			response = await resp.read()
 			res_len = len(response)
-			result = results[url]
-			result["status"] = ResultStatus.SUCCESS
-			result["transferred"] = res_len
+			test_item.status = ResultStatus.SUCCESS
+			test_item.transferred = res_len
+			test_item.time = time.time() - start_time
 			if verbose:
-				results[url]["time"] = time.time() - start_time
-				print(f"{url}: - Transferred: {res_len} Bytes. Time: {results[url]['time']:.4f}s.")
+				print(f"{test_item.url} - Transferred: {res_len} Bytes. Time: {test_item.time:.4f}s.")
 	except Exception as e:
 		global errorNum
-		results[url]["time"] = time.time() - start_time
-		results[url]["status"] = ResultStatus.ERROR
+		test_item.time = time.time() - start_time
+		test_item.status = ResultStatus.ERROR
 		if verbose:
-			print(f"Error: {url} - {e.__class__}")
+			print(f"Error: {test_item.url} - {e.__class__}")
+
+	return test_item
 
 
-async def spawn_requests(urls: list[str]) -> float:
-	start = time.time()
-
+async def spawn_requests(test_items: list[TestItem]) -> list[TestItem]:
 	async with ClientSession() as session:
-		await asyncio.gather(*[get_http_resp(url, session) for url in urls])
+		tasks = [get_http_resp(session, test_item) for test_item in test_items]
+		try:
+			await asyncio.wait_for(asyncio.gather(*tasks), timeout=5)
+		except asyncio.TimeoutError:
+			await asyncio.gather(*tasks, return_exceptions=True)
 
-	duration = time.time() - start
-
-	if verbose:
-		print(f"Requested {len(urls)} websites in {duration:.2f}s.")
-
-	return duration
+	return test_items
 
 
-def eval() -> Stats:
+def eval(results: list[TestItem]) -> Stats:
 	stats: Stats = {"time": 0.0, "successes": 0, "errors": 0, "timeouts": 0, "transferred": 0}
-	for res in results.values():
-		stats["transferred"] += res["transferred"]
-		stats["successes"] += res["status"] == ResultStatus.SUCCESS
-		stats["errors"] += res["status"] == ResultStatus.ERROR
-		stats["timeouts"] += res["status"] == ResultStatus.TIMEOUT
+	for res in results:
+		stats["transferred"] += res.transferred
+		stats["successes"] += res.status == ResultStatus.SUCCESS
+		stats["errors"] += res.status == ResultStatus.ERROR
+		stats["timeouts"] += res.status == ResultStatus.PENDING
 
-		summary["transferred"] += res["transferred"]
-		summary["successes"] += res["status"] == ResultStatus.SUCCESS
-		summary["errors"] += res["status"] == ResultStatus.ERROR
-		summary["timeouts"] += res["status"] == ResultStatus.TIMEOUT
+		summary["transferred"] += res.transferred
+		summary["successes"] += res.status == ResultStatus.SUCCESS
+		summary["errors"] += res.status == ResultStatus.ERROR
+		summary["timeouts"] += res.status == ResultStatus.PENDING
 
 	stats['transferred'] = stats['transferred'] / (1024 * 1024)
 
@@ -86,23 +95,29 @@ def eval() -> Stats:
 
 def main():
 	urls = prep_urls()
+	# prepare test items
+	test_items = [TestItem(url=url, status=ResultStatus.PENDING, transferred=0, time=0.0) for url in urls]
 
 	print("Starting requests...")
 
 	for i in range(iterations):
 		print(f"Run: {i + 1}/{iterations}")
 
-		for url in urls:
-			results[url] = {"status": ResultStatus.PENDING, "transferred": 0, "time": 0.0}
+		start = time.time()
+		results = asyncio.run(spawn_requests(test_items))
+		duration = time.time() - start
 
-		duration = asyncio.run(spawn_requests(urls))
-
-		stats = eval()
+		stats = eval(results)
 		stats["time"] = duration
 		summary["time"] += duration
 
-		output = f"{i + 1}: Time: {stats['time']:.2f}s. Sent: {stats['successes'] + stats['errors'] + stats['timeouts']}. Successes: {stats['successes']}. Errors: {stats['errors']}. Timeouts: {stats['timeouts']}. Transferred: {stats['transferred']:.2f} MB ({stats['transferred'] / stats['time']:.2f} MB/s)."
-
+		output = (f"{i + 1}: Time: {stats['time']:.2f}s. "
+			f"Sent: {stats['successes'] + stats['errors'] + stats['timeouts']}. "
+			f"Successes: {stats['successes']}. "
+			f"Errors: {stats['errors']}. "
+			f"Timeouts: {stats['timeouts']}. "
+			f"Transferred: {stats['transferred']:.2f} MB "
+			f"({stats['transferred'] / stats['time']:.2f} MB/s).")
 		outputs.append(output)
 
 		if verbose:
@@ -111,17 +126,20 @@ def main():
 	if len(outputs) <= 1 and verbose:
 		return
 
-	print(f"{seperator}")
+	print(seperator)
 
 	for output in outputs:
 		print(output)
 
 	summary['transferred'] = summary['transferred'] / (1024 * 1024)
 
-	print(f"""{seperator}
-Runs: {iterations}. Average Time: {summary['time'] / float(len(outputs)):.2f}s. Total Errors: {summary['errors']}. Total Timeouts: {summary['timeouts']}. Transferred: {summary['transferred']:.2f} MB ({summary['transferred']/summary['time']:.2f} MB/s).
-{seperator}
-""")
+	print(seperator)
+	print(f"Runs: {iterations}. "
+		f"Average Time: {summary['time'] / float(len(outputs)):.2f}s. "
+		f"Total Errors: {summary['errors']}. "
+		f"Total Timeouts: {summary['timeouts']}. "
+		f"Transferred: {summary['transferred']:.2f} MB ({summary['transferred']/summary['time']:.2f} MB/s).")
+	print(seperator)
 
 
 main()
